065e15c0fa1f3c476cf9397c4b3314a4a9ca605e
RawanOs <89946063+RawanOs@users.noreply.github.com>
2023-06-03 22:21:21 +0300

    Adding Card

--- MapCard/ContentView.swift
+++ MapCard/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+                .shadow(radius: 1)
+
+            VStack {
+                Text("King Fahad Gate")
+                    .fontWeight(.semibold)
+                    .padding(.trailing,50)
+                HStack {
+                    Image(systemName: "clock")
+                        .resizable()
+                        .frame(width: 18,height:18)
+                    Text("12 Min")
+                        .foregroundColor(Color.gray)
+                }
+                .padding(.trailing,100)
+                HStack {
+                    Image(systemName: "clock")
+                        .resizable()
+                        .frame(width: 18,height:18)
+                        
+                    Text("1.2 KM")
+                        .foregroundColor(Color.gray)
+                }
+                .padding(.trailing,100)
+        
+              }
+            .padding(.trailing,120)
+            
+            
+            HStack{
+                Button(action: {
+                 
+                }, label: {
+                    Text("Start")
+                        .bold()
+                        .foregroundColor(Color.white)
+                        .frame(width: 98, height: 41)
+                        .background(Color("Color"))
+                        .cornerRadius(16)
+                })
+            }
+            .padding(.leading,230)
+            .padding(.bottom,-180)
+            
+            
         }
-        .padding()
+        .frame(width: 343, height: 132)
+        .onTapGesture {
+    
+        }
     }
 }
 
