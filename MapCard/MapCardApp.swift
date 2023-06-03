d97bdf6aafcb1cb943cba855f026aeb46ae234c0
RawanOs <89946063+RawanOs@users.noreply.github.com>
2023-06-03 17:29:09 +0300

    Initial Commit

---
+++ MapCard/MapCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapCardApp.swift
+//  MapCard
+//
+//  Created by Rawan on 14/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MapCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
